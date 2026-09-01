3ec3fe695cf0efdc31e435a7ba5688dd62d39c29
Rohan Solkar <Rohan.Solkar@teknopoint.local>
2026-09-01 12:14:40 +0530

    API URL uodated 1

--- my-next-app/utils/AxiosAPICall.js
+++ my-next-app/utils/AxiosAPICall.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URI || "http://localhost:5000",
+  baseURL: process.env.API_BASE_URI || "http://localhost:5000",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
